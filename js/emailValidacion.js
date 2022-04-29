b7f29f76cc9a3239c3dd8b5dbe14b7326192df64
AlejandroOrdenes <alejandro.ordenes.o@gmail.com>
2022-04-29 01:21:48 -0400

    validacionUsuario

---
+++ js/emailValidacion.js
@@ -0,0 +1,8 @@
+
+
+function isEmail(email) {
+    var EmailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return EmailRegex.test(email);
+  }
+
+export function isEmail(email);
--- js/usuario.js
+++ js/usuario.js
@@ -3,37 +3,48 @@ $(document).ready(function(){
 
         e.preventDefault();
 
+        let email = $("#inputEmail").val().trim();
+
+        function isEmail(email) {
+            var EmailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            return EmailRegex.test(email);
+          }
+
         if($("#inputNombre").val().trim().length==0 ){
             $("#inputNombre").css("border", "1px solid #ff3838");
+            $("#inputNombre").focus(function(){
+                $("#inputNombre").css("border", "1px solid #ced4da");
+            });
+        } else {
+            $("#inputNombre").css("border", "1px solid greenyellow");
         }
 
-        if($("#inputEmail").val().trim().length==0 ){
+        if(isEmail(email)){
+            $("#inputEmail").css("border", "1px solid greenyellow");
+            $("#inputEmail").focus(function(){
+                $("#inputEmail").css("border", "1px solid #ced4da");
+            });
+        } else {
             $("#inputEmail").css("border", "1px solid #ff3838");
         }
 
         if($("#inputPass").val().trim().length==0 ){
             $("#inputPass").css("border", "1px solid #ff3838");
+            $("#inputPass").focus(function(){
+                $("#inputPass").css("border", "1px solid #ced4da");
+            });
+        } else {
+            $("#inputPass").css("border", "1px solid greenyellow");
         }
 
         if($("#inputRepPass").val().trim().length==0 ){
             $("#inputRepPass").css("border", "1px solid #ff3838");
+            $("#inputRepPass").focus(function(){
+                $("#inputRepPass").css("border", "1px solid #ced4da");
+            });
+        } else {
+            $("#inputRepPass").css("border", "1px solid greenyellow");
         }
-
-        $("#inputNombre").focus(function(){
-            $("#inputNombre").css("border", "1px solid #ced4da");
-        });
-
-        $("#inputEmail").focus(function(){
-            $("#inputEmail").css("border", "1px solid #ced4da");
-        });
-
-        $("#inputPass").focus(function(){
-            $("#inputPass").css("border", "1px solid #ced4da");
-        });
-
-        $("#inputRepPass").focus(function(){
-            $("#inputRepPass").css("border", "1px solid #ced4da");
-        });
 
     });
 });

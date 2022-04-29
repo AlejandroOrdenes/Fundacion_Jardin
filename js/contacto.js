8419214dc0b44e9629c9e453c3fcb5e8b7b04563
AlejandroOrdenes <alejandro.ordenes.o@gmail.com>
2022-04-28 21:47:17 -0400

    validaciónContacto

--- js/contacto.js
+++ js/contacto.js
@@ -4,19 +4,22 @@ $(document).ready(function(){
         e.preventDefault();
 
         if($("#inputEmail").val().trim().length==0 ){
-            $("inputEmail").attr("placeholder", "Ingrese Email Valido!!!")
-            $("#inputEmail").css("border", "3px solid #ff3838");
+            document.getElementById("inputEmail").setAttribute("placeholder", "Ingrese Email Valido");
+            $("#inputEmail").css("border", "1px solid #ff3838");
         }
         if($("#inputText").val().trim().length==0){
-            $("#inputText").css("border", "3px solid #ff3838");
+            document.getElementById("inputText").setAttribute("placeholder", "Ingrese un Mensaje");
+            $("#inputText").css("border", "1px solid #ff3838");
         }
 
         $("#inputEmail").focus(function(){
+            document.getElementById("inputEmail").setAttribute("placeholder", "");
             $("#inputEmail").css("border", "1px solid #ced4da");
         });
 
         $("#inputText").focus(function(){
             $("#inputText").css("border", "1px solid #ced4da");
+            document.getElementById("inputText").setAttribute("placeholder", "");
         });
     });
 });

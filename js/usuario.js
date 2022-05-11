1b4ca36c022119b568fa8f49fd437726a3e356f2
AlejandroOrdenes <alejandro.ordenes.o@gmail.com>
2022-05-10 21:46:58 -0400

    usuario

--- js/usuario.js
+++ js/usuario.js
@@ -1,13 +1,11 @@
 import { isEmail, nameValidate, addressValidate, passwordValidate, pass2Validate, get, valid, getFocus } from "./validaciones.js"
 
-let ids = ["inputNombre", "inputAddress", "inputEmail", "inputPass", "inputPass2" ]
+let pass2 = pass2Validate( passwordValidate(get("inputPass").value), passwordValidate(get("inputPass2").value )) 
 
 const validacion = (e) => {
 
     e.preventDefault();
 
-    let pass2 = pass2Validate( passwordValidate(get("inputPass").value), passwordValidate(get("inputPass2").value )) 
-    
     valid(nameValidate( get("inputNombre").value ), "inputNombre", "errorNombre" )
     valid(addressValidate( get("inputAddress").value ), "inputAddress", "errorDireccion" )
     valid(isEmail( get("inputEmail").value ), "inputEmail", "errorEmail" )
@@ -18,6 +16,27 @@ const validacion = (e) => {
 
 }
 
+get("inputNombre").addEventListener("blur", function( event ) {
+    valid(nameValidate( get("inputNombre").value ), "inputNombre", "errorNombre" )
+  }, true);
+
+get("inputAddress").addEventListener("blur", function( event ) {
+    valid(addressValidate( get("inputAddress").value ), "inputAddress", "errorDireccion" )
+}, true);
+
+get("inputEmail").addEventListener("blur", function( event ) {
+    valid(isEmail( get("inputEmail").value ), "inputEmail", "errorEmail" )
+}, true);
+
+get("inputPass").addEventListener("blur", function( event ) {
+    valid(passwordValidate( get("inputPass").value ), "inputPass", "errorPass" )
+    
+}, true);
+
+get("inputPass2").addEventListener("blur", function( event ) {
+    pass2 = pass2Validate( passwordValidate(get("inputPass").value), passwordValidate(get("inputPass2").value )) 
+    valid(pass2, "inputPass2", "errorPass2" )
+}, true);
+
 get("botonRegistrarse").addEventListener('click', validacion);
-getFocus(ids, validacion);
 

462a7cb8ecd3242d89f63c4225a712929815baba
AlejandroOrdenes <alejandro.ordenes.o@gmail.com>
2022-05-10 22:03:14 -0400

    usuario

--- js/contacto.js
+++ js/contacto.js
@@ -21,7 +21,23 @@ const validacion = (e) => {
         count += 1
     }
 
+    if(count == 2){
+        window.alert("Mensaje Enviado!")
+    }
+
 }
 
+get("inputEmailCont").addEventListener("blur", function( event ) {
+    if(valid(isEmail( get("inputEmailCont").value ), "inputEmailCont", "errorEmailCont" ) == false){
+        get("emailHelpCont").style.display = "none"
+    }
+}, true);
+
+get("inputText").addEventListener("blur", function( event ) {
+    if(valid(messageValidate( get("inputText").value ), "inputText", "errorMensajeCont" ) == false){
+        get("mensajeCont").style.display = "none"
+    }
+}, true);
+
 get("botonEnviar").addEventListener('click', validacion)
-getFocus(ids, validacion)
+
--- js/login.js
+++ js/login.js
@@ -1,14 +1,26 @@
 import { isEmail, passwordValidate, get, valid, getFocus} from "./validaciones.js"
 
-let ids = ["floatingInput", "floatingPassword"]
-
+let count = 0
 const validacion = (e) => {
 
     e.preventDefault()
 
-    valid(isEmail( get("floatingInput").value ), "floatingInput", "errorEmailLog" )
-    valid(passwordValidate( get("floatingPassword").value ), "floatingPassword", "errorPassLog" )
+    if(valid(isEmail( get("floatingInput").value ), "floatingInput", "errorEmailLog" )){
+        count +=1
+    }
+    if(valid(passwordValidate( get("floatingPassword").value ), "floatingPassword", "errorPassLog" )){
+        count += 1
+    }
+   
 }
 
+get("floatingInput").addEventListener("blur", function( event ) {
+    valid(isEmail( get("floatingInput").value ), "floatingInput", "errorEmailLog" )
+}, true);
+
+get("floatingPassword").addEventListener("blur", function( event ) {
+    valid(passwordValidate( get("floatingPassword").value ), "floatingPassword", "errorPassLog" )
+    
+}, true);
+
 get("loginButton").addEventListener('click', validacion);
-getFocus(ids, validacion)
